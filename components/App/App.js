4d26a53ddf0f48d91e04527817dfae41cd12e0eb
Avinash110 <maheshwariavinash50@gmail.com>
2020-05-13 19:37:38 -0700

    user typing bug solved

--- components/App/App.js
+++ components/App/App.js
@@ -11,75 +11,77 @@ import UserForm from "../Users/UserForm/UserForm.js";
 import UserList from "../Users/UserList/UserList.js";
 
 const eventToFunctionMapping = [
-	{'event': 'connect', 'function': 'connect'},
-	{'event': 'disconnect', 'function': 'disconnect'},
-	{'event': 'messageAdded', 'function': 'onMessageAdded'},
-	{'event': 'userJoined', 'function': 'onUserJoined'},
-	{'event': 'userTyping', 'function': 'userTyping'}
+    { 'event': 'connect', 'function': 'connect' },
+    { 'event': 'disconnect', 'function': 'disconnect' },
+    { 'event': 'messageAdded', 'function': 'onMessageAdded' },
+    { 'event': 'userJoined', 'function': 'onUserJoined' },
+    { 'event': 'userTyping', 'function': 'userTyping' }
 ];
 export default class App extends React.Component {
-		constructor(props) {
-				super(props);
-				this.state = {
-						status: "Disconnected",
-						messages: [{
-								timeStamp: Date.now(),
-								text: "Welcome to Socket Chat"
-						}],
-						users: [],
-						user: '',
-						userTyping: ''
-				};
-		}
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: "Disconnected",
+            messages: [{
+                timeStamp: Date.now(),
+                text: "Welcome to Socket Chat"
+            }],
+            users: [],
+            user: '',
+            userTyping: ''
+        };
+    }
 
-		componentWillMount() {
-				this.socket = io(location.origin);
+    componentWillMount() {
+        this.socket = io(location.origin);
 
-				eventToFunctionMapping.forEach((element, index) => {
-					this.socket.on(element.event, this[element.function]);
-				});
-		}
+        eventToFunctionMapping.forEach((element, index) => {
+            this.socket.on(element.event, this[element.function]);
+        });
+    }
 
-		userTyping = (payload) => {
-				this.setState({userTypingId: payload.id, userTyping: payload.user});
-		}
+    userTyping = (payload) => {
+    	if(payload.id != this.socket.id){
+        	this.setState({ userTypingId: payload.id, userTyping: payload.user });
+    	}
+    }
 
-		onUserJoined = (users) => {
-				this.setState({ users: users });
-		}
+    onUserJoined = (users) => {
+        this.setState({ users: users });
+    }
 
-		onMessageAdded = (message) => {
-				this.setState({ messages: this.state.messages.concat(message) });
-		}
+    onMessageAdded = (message) => {
+        this.setState({ messages: this.state.messages.concat(message) });
+    }
 
-		connect = () => {
-				this.setState({ status: "Connected" });
-				console.log("Connected:", this.socket.id);
-		}
+    connect = () => {
+        this.setState({ status: "Connected" });
+        console.log("Connected:", this.socket.id);
+    }
 
-		disconnect = (users) => {
-				this.setState({ status: "Disconnected", users: users });
-		}
+    disconnect = (users) => {
+        this.setState({ status: "Disconnected", users: users });
+    }
 
-		emit = (eventName, payload) => {
-				this.socket.emit(eventName, payload);
-		}
+    emit = (eventName, payload) => {
+        this.socket.emit(eventName, payload);
+    }
 
-		setUser = (user) => {
-				this.setState({ user: user });
-		}
+    setUser = (user) => {
+        this.setState({ user: user });
+    }
 
-		render() {
-				if (this.state.user == '') {
-						return (
-								<UserForm 
+    render() {
+        if (this.state.user == '') {
+            return (
+                <UserForm 
 									emit={this.emit} 
 									setUser={this.setUser}
 								/>
-						);
-				} else {
-						return (
-								<div className="row">
+            );
+        } else {
+            return (
+                <div className="row">
 									<div className="col-md-4 offset-md-1">
 										<UserList 
 											{...this.state}
@@ -89,14 +91,14 @@ export default class App extends React.Component {
 										<MessageList 
 											{...this.state}
 										/>
-										<span className="current-user-typing">{this.state.userTypingId != this.socket.id && this.state.userTyping ? this.state.userTyping + " is typing ..." : ""} </span>
+										<span className="current-user-typing">{this.state.userTyping ? this.state.userTyping + " is typing ..." : ""} </span>
 										<MessageForm
 											emit={this.emit}
 											{...this.state}
 										/>
 									</div>
 								</div>
-						);
-				}
-		}
+            );
+        }
+    }
 }
